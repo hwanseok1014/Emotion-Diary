866618f10532579474d1c5b139039a42ded43fd6
Lee hwan seuk <sioo1014@naver.com>
2025-02-25 01:56:14 +0900

    feat(MockData):일기 MockData 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 import './App.css';
-import {Routes, Route, Link} from "react-router-dom";
+import {Routes, Route, Link, data} from "react-router-dom";
+import {useRef} from 'react';
 
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import Editor from './pages/Editor';
 import Notfound from './pages/Notfound';
+import { useReducer } from 'react';
 
-import Button from './components/Button';
-import Header from './components/Header';
+
+const MockData = [{
+  id:1,
+  createDate: new Date().getTime(),
+  emotionId: 1,
+  content: "1번 일기 내용",
+}, {
+  id:2,
+  createDate: new Date().getTime(),
+  emotionId: 2,
+  content: "2번 일기 내용",
+}]
+
 
 function App() {
+  const [data,dispatch] =useReducer(reducer,MockData);
+  const idRef =useRef(3);
+
+
   return (
   <>
-    
+      <button onClick={()=>onCreate(new Date().getTime(),1,'Hello')}>일기 추가 테스트</button>
+      <button onClick={()=>onUpdate(1,new Date().getTime(),3,'수정된 일기입니다.')}>일기 수정 테스트</button>
+      <button onClick={()=>onDelete(1)}>일기 삭제 테스트</button>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/new" element={<New/>}/>
--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 const Editor = ()=>{
     const params = useParams();
-    return <div>{params.id}번</div>
+    return <div>{params.id}번 일기 수정 페이지입니다.</div>
 }
 
 export default Editor;
