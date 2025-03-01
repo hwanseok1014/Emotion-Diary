ef76e35408325c8b4eaf71419a6275936b9baf07
Lee hwan seuk <sioo1014@naver.com>
2025-03-02 01:36:58 +0900

    feat(Home): 월 단위 변경 기능 추가(이전/다음)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,29 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 
+import { useState } from "react";
+
 const Home =( ) =>{
+    const [pivoDate, setPivoDate] =useState(new Date());
+
+    const onIncreaseMonth = ()=>{
+        setPivoDate(new Date(pivoDate.getFullYear(),pivoDate.getMonth()+1));
+    };
+
+    const onDecreaseMonth = ()=> {
+        setPivoDate(new Date(pivoDate.getFullYear(),pivoDate.getMonth()-1));
+    };
+
     return(
     <div>
-        <Header title='2025년 2월'
-        leftChild={<Button text={'<'}/>}
-        rightChild={<Button text={'>'}/>}
+        <Header title={`${pivoDate.getFullYear()}년 ${pivoDate.getMonth()+1}월`}
+        leftChild={<Button text={'<'} 
+        onClick={()=>onDecreaseMonth()}/>}
+        rightChild={<Button text={'>'} 
+        onClick={()=>onincreaseMonth()}/>}
         />
         <DiaryList>
-            
+
         </DiaryList>
 
     </div>
