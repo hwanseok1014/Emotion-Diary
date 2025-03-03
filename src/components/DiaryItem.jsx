e7a243a104fc270bca8044f877451a5a942cb151
Lee hwan seuk <sioo1014@naver.com>
2025-03-04 00:36:20 +0900

    feat(home):일기 리스트 및 수정 버튼 클릭 시 페이지 이동 구현

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -2,14 +2,18 @@ import './DiaryItem.css';
 
 import getEmotionImage from '../utill/get-emotion-image';
 import Button from './Button';
+import { useNavigate } from 'react-router-dom';
 
 const DiaryItem = ({id, emotionId, createDate,content})=>{
+    const navigation =useNavigate();
 
     return<div className='DiaryItem'>
-        <div className={`Image_section Image_section_${emotionId}`}>
+        <div className={`Image_section Image_section_${emotionId}`}
+        onClick={()=>{navigation(`/diary/${id}`)}}>
             <img src={getEmotionImage(emotionId)}/>
         </div>
-        <div className='Info_section'>
+        <div className='Info_section'
+        onClick={()=>{navigation(`/diary/${id}`)}}>
             <div className='created_date'>
               {new Date(createDate).toLocaleDateString()}
             </div>
@@ -18,7 +22,7 @@ const DiaryItem = ({id, emotionId, createDate,content})=>{
             </div>
         </div>
         <div className='Button_section'>
-            <Button text={'수정하기'}/>
+            <Button text={'수정하기'} onClick={()=>{navigation(`/edit/${id}`)}}/>
         </div>
     </div>
 
