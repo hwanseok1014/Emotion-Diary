b19bf1fa62449ac83ecc36cc741d074ba1f4d7f7
Lee hwan seuk <sioo1014@naver.com>
2025-03-04 22:21:42 +0900

    feat(new): 새 일기 페이지 UI 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './Editor.css';
 import EmotionItem from './EmotionItem';
+import Button from './Button';
 
 const emotionList =[{
     emotionId:1,
@@ -31,7 +32,7 @@ const Editor =()=>{
     return <div className='Editor'>
         <section className='date_section'>
             <h4>오늘의 날짜</h4>
-            <input type='date'></input>        
+            <input type='date'/>        
         </section>
         
         <section className='emotion_section'>
@@ -42,15 +43,17 @@ const Editor =()=>{
                 {...item}
                 isSelected={item.emotionId === emotionId}/>)}
             </div>
-
         </section>
         
         <section className='content_section'>
-
+            <h4>오늘의 일기</h4>
+            <textarea placeholder='오늘은 어땠나요?'/>
         </section>
 
         <section className='button_section'>
-
+            <Button text={'취소하기'}/>
+            <Button text={'작성완료'} 
+            type={'POSITIVE'}/>
         </section>
 
     </div>
